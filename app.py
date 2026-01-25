12df66bf518133db3fc3db382d6307ef609e6aba
Constantin Bender <constantinbender51@gmail.com>
2026-01-25 09:19:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 # --- Parameters ---
 SYMBOL = 'BTC/USDT'
 TIMEFRAME = '1h'
-START_STR = '2023-01-01 00:00:00'
+START_STR = '2025-01-01 00:00:00'
 END_STR = None 
 B_SPLIT = 0.70  # 70% Training
 C_TOP = 0.20  # Top 20% frequent sequences
